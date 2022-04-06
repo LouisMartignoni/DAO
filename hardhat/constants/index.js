5fbbfe5062d215ba1f7dace2f827e694b2b61bc0
LouisMartignoni <martignonilouis@gmail.com>
2022-04-06 22:28:51 +0200

    fitst commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x4A95fCe270d3d513EFd524C80b0511536A887817";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
